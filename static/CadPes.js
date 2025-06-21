8f78f31a031d32ab01eebbcb09236470c8dd37f9
João Torezan <jabtorezan@gmail.com>
2025-06-21 17:00:45 -0300

    added password verify

--- static/CadPes.js
+++ static/CadPes.js
@@ -1,8 +1,33 @@
-function User(){
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form.info");
+
+    form.addEventListener("submit", function (event) {
+        const nome = document.getElementById("nome").value;
+        const username = document.getElementById("username").value;
+        const senha = document.getElementById("passwd").value;
+        const confirmarSenha = document.getElementById("passwdCkeck").value;
+
+        if (nome === ""  username === ""  senha === "" || confirmarSenha === "") {
+            event.preventDefault();
+            mostrarAlerta("Preencha todos os campos!");
+            return;
+        }
+
+        if (senha !== confirmarSenha) {
+            event.preventDefault();
+            mostrarAlerta("As senhas não coincidem!");
+            return;
+        }
+
+    });
+});
+
+
+function mostrarAlerta(mensagem) {
     Swal.fire({
-        title: "Usuário",
-        text: "Usuário cadastrado com sucesso",
-        icon: "sucess",
+        title: "Campo invalido",
+        text: mensagem,
+        icon: "error",
         confirmButtonText: "OK",
         confirmButtonColor: "#3085d6",
     });
